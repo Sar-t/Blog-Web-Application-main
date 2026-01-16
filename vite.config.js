d24fc6779c09546e15b734256395d705dd90ca70
Sarthak Tomar <sarthaktomar345@gmail.com>
2026-01-16 18:51:02 +0530

    feat: Update Vite proxy configuration to use environment variable for API base URL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api/v1':{
-        target: `http://localhost:${process.env.PORT || 8000}`,
+        target: `${process.env.VITE_API_BASE_URL}`,
         changeOrigin: true
       },
     },
